e8e080eae08ee62bc5bd30f9125a30f9562d3055
Charles Reed <reecw-27@rhodes.edu>
2026-02-07 14:12:15 -0600

    committing to debug on my computer

--- ping.c
+++ ping.c
@@ -22,6 +22,7 @@ int main(int argc, char **argv) {
   char *ponghost = strdup("localhost"); // default host
   char *pongport = strdup(PORTNO);      // default port
   int arraysize = 100;                  // default packet size
+//  int verbose = 0;
 
   while ((ch = getopt(argc, argv, "h:n:p:")) != -1) {
     switch (ch) {
@@ -37,6 +38,9 @@ int main(int argc, char **argv) {
     case 's':
       arraysize = atoi(optarg);
       break;
+    //case 'v':
+     // verbose = 1;
+     // break;
     default:
       fprintf(stderr, "usage: ping [-h host] [-n #pings] [-p port] [-s size]\n");
     }
@@ -45,6 +49,74 @@ int main(int argc, char **argv) {
   // UDP ping implemenation goes here
   printf("nping: %d arraysize: %d errors: %d ponghost: %s pongport: %s\n",
       nping, arraysize, errors, ponghost, pongport);
+
+  char arr[arraysize]; //= 200; 
+ // for (int i = 0; i < sizeof(arr); i++){
+    //arr[i] = 200; 
+  //}
+ 
+  //if (verbose) printf("ping: attempting to alloc and memset arr\n");
+ /* char *arr = malloc(arraysize * sizeof(char)); 
+  if (arr == NULL){
+    perror("Failed to alloc array"); 
+    exit(1);
+  }
+  memset(arr, 200, arraysize);
+  */
+  for (size_t cur = 0; cur < arraysize; cur++){
+    arr[cur] = (char)200;
+  }
+  printf("set char array to 200\n");
+  int sockfd; 
+ // int numbytes; 
+  int sadderinfo;
+  struct addrinfo hints, *servinfo, *p; // int rv;
+//  char s[INET6_ADDRSTRLEN];
+  hints.ai_family = AF_INET; 
+  hints.ai_socktype = SOCK_DGRAM; 
+  hints.ai_protocol = IPPROTO_UDP; 
+ if ((sadderinfo = getaddrinfo(ponghost, pongport, &hints, &servinfo)) == -1)
+ {
+   perror("ping: addrinfo"); 
+   return 1;
+ }
+  printf("ping: set addrinfo, attempting to call socket\n"); 
+ // sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+ for (p = servinfo; p != NULL; p = p->ai_next) {
+   if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+     printf("Client: connected.\n");
+     continue;
+   }
+  if (sockfd == -1) {
+    perror("Socket not found.");
+    exit(1);
+  }
+ }
+  printf("Client: Trying to send to %d.\n", sadderinfo); 
+//  int totalTimeCounter = 0;
+ // int localTimeCounter = 0;
+  char recvArr[arraysize]; 
+  for (int pingnum = 0; pingnum < nping; pingnum++)
+  {
+//    localTimeCounter = 0; 
+    send(sockfd, arr, sizeof(arr), 0);
+    recv(sockfd, recvArr, sizeof(arr), 0);
+    for (int i = 0; i < arraysize; i++)
+    {
+      if ((int)recvArr[i] != 201){
+        perror("ping: array did not arrive back with size 201"); 
+        break;
+      }
+    }
+
+
+
+
+
+
+  }
+
+
 
   return 0;
 }
--- pong.c
+++ pong.c
@@ -1,6 +1,6 @@
 /*
  * pong.c - UDP ping/pong server code
- *          author: <your name>
+ *          author: <Charles Reed>
  */
 #include <netdb.h>
 #include <stdio.h>
@@ -35,9 +35,75 @@ int main(int argc, char **argv) {
     }
   }
 
+ 
+
   // pong implementation goes here.
   printf("nping: %d pongport: %s\n", nping, pongport);
 
+  struct addrinfo  hints, *servinfo, *p;
+  int sadderinfo; 
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM; 
+  hints.ai_protocol = IPPROTO_UDP; 
+  int sockfd;
+//  int bindret;
+  
+  sadderinfo = getaddrinfo(NULL, pongport, &hints, &servinfo);
+/*
+  for(p = servinfo; p!= NULL; p = p->ai_next)
+  {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+      perror("server: socket\n");
+      continue;
+    }
+    if ((bind(sockfd, p->ai_addr, p->ai_addrlen)) == -1){
+      perror("server: bind\n");
+      continue;
+  }
+*/
+  //bind(sockfd, p->ai_addr, p->ai_addrlen);
+  //sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol); 
+/*   for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -2) {
+            perror("server: socket");
+            continue;
+        }
+   }
+
+  bindret = bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
+
+*/
+  printf("pong: sadderinfo: %d", sadderinfo);
+  printf("waiting for connections.");
+  while(1){
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+      if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -2){
+        perror("Server: didn't bind to socket");
+        continue;
+
+      }
+      if ((bind(sockfd, p->ai_addr, p->ai_addrlen)) == -1){
+        perror("pong: bind error");
+        close(sockfd);
+        continue;
+      }
+    } 
+
+
+
+    // int newsock = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size
+    for (int i = 0; i < nping; i++){
+      char *cbuf; 
+      recv(sockfd, &cbuf, sizeof(cbuf), 0); 
+      printf("pong[%d]: recieved packet from %d", i,sockfd );
+      for (int v = 0; v < sizeof(cbuf); v++)
+      {
+         cbuf[v]++;
+        }
+      send(sockfd, &cbuf, sizeof(cbuf), 0);
+    }
+  }
   return 0;
 }
 
